8a6301adf77eef295cd9ccb09ba70f0959f28d64
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2019-11-18 22:12:55 -0300

    Classe Disciplina

---
+++ TrabalhoFinalOO/src/Instituicao/Disciplina.java
@@ -0,0 +1,38 @@
+package instituicao;
+import javax.swing.JOptionPane;
+public class Disciplina {
+	public String nomeDisciplina;
+	public int numCreditos;
+	public static void criarDisciplina(int auxDisciplina) {
+		Disciplina dc[] = new Disciplina[10];
+		dc[auxDisciplina].nomeDisciplina = JOptionPane.showInputDialog("Digite o nome da disciplina");
+		dc[auxDisciplina].nomeDisciplina = validaNomeDisciplina(dc[auxDisciplina].nomeDisciplina);
+		dc[auxDisciplina].numCreditos = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de créditos da disciplina"));
+	}
+	public static void cadastrarTurmas() {
+		
+	}
+	public static void listarTurmas() {
+		
+	}
+	//Objetivo: Validar o nome da Disciplina, pois ele não pode estar em branco
+	public static String validaNomeDisciplina(String nomeDisciplina){
+		while(nomeDisciplina == "") {
+			//exception
+			nomeDisciplina = JOptionPane.showInputDialog("Digite o nome da disciplina");
+		}
+		return nomeDisciplina;
+	}
+	public String getNomeDisciplina() {
+		return nomeDisciplina;
+	}
+	public void setNomeDisciplina(String nomeDisciplina) {
+		this.nomeDisciplina = nomeDisciplina;
+	}
+	public int getNumCreditos() {
+		return numCreditos;
+	}
+	public void setNumCreditos(int numCreditos) {
+		this.numCreditos = numCreditos;
+	}
+}
