51123d21b672144dca8ddd3f07d4371a95a36724
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2019-11-22 21:21:15 -0300

    Update ProjetoMain.java

--- TrabalhoFinalOO/src/instituicao/ProjetoMain.java
+++ TrabalhoFinalOO/src/instituicao/ProjetoMain.java
@@ -3,12 +3,16 @@ package instituicao;
 import java.util.ArrayList;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 public class projetoMain {
 =======
 import javax.swing.JOptionPane;
 
 public class ProjetoMain {
 >>>>>>> a956da93f84a0f9359d57d8196ac611928f42e1e
+=======
+public class ProjetoMain {
+>>>>>>> parent of a28e48c... Update ProjetoMain.java
 
 	public static void main(String[] args){
 		
@@ -18,6 +22,5 @@ public class ProjetoMain {
 		
 		//Criação do primeiro Campus:
 		listaCampus = classCampus.criarCampus(listaCampus);
-
 	}
 }
