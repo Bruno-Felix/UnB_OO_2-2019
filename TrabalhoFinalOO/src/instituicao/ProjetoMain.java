76571c8e9c1ad7b8cfafcdbcae897691e039e174
Bruno-Felix <balvesfelix@gmail.com>
2019-11-22 21:23:31 -0300

    Resolvendo Conflitos

--- TrabalhoFinalOO/src/instituicao/ProjetoMain.java
+++ TrabalhoFinalOO/src/instituicao/ProjetoMain.java
@@ -2,17 +2,9 @@ package instituicao;
 
 import java.util.ArrayList;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-public class projetoMain {
-=======
 import javax.swing.JOptionPane;
 
 public class ProjetoMain {
->>>>>>> a956da93f84a0f9359d57d8196ac611928f42e1e
-=======
-public class ProjetoMain {
->>>>>>> parent of a28e48c... Update ProjetoMain.java
 
 	public static void main(String[] args){
 		
