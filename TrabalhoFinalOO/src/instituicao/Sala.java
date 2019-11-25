318a4ff4d7e1f6486375999e581f03079312045f
Enzo Gabriel <eggqsaraiva@gmail.com>
2019-11-25 12:08:00 -0300

    criando classe sala

---
+++ TrabalhoFinalOO/src/instituicao/Sala.java
@@ -0,0 +1,20 @@
+package instituicao;
+
+public class Sala {
+	public String nomeDaSala;
+	public int ocupacaoDaSala;
+	public String tipoDeAtividade;
+	public boolean possuiArCondicionado;
+	public boolean possuiProjetor;
+	public boolean possuiTelevisao;
+	
+	public void cadastrarSala() {
+		
+	}
+	
+	public void verificarAtividadeAtribuida() {
+		
+	}
+	
+	
+}
