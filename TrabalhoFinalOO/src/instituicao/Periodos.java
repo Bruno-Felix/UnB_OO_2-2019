06e2b4689cf26747d4c30a8eeec0a7c0c84ee5c8
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2019-12-04 20:01:22 -0300

    Create Periodos.java
    criando a classe períodos

---
+++ TrabalhoFinalOO/src/instituicao/Periodos.java
@@ -0,0 +1,18 @@
+package instituicao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Periodos {
+	public int numeroDePeriodos;
+	public String diaDaSemana;
+	public String horaDoDia;
+	List<Periodos> listaSegunda = new ArrayList<Periodos>();
+	static List<Disciplina> listaDisciplina = new ArrayList<Disciplina>();
+	public void cadastrarPeriodos() {
+		
+	}
+	public void listarPeriodos() {
+		
+	}
+}
