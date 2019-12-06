a2533a86db0e68882ea4696191e75221fefe4e0d
Bruno-Felix <balvesfelix@gmail.com>
2019-12-06 16:05:10 -0300

    Criando método Aluno Caso Teste para o método cadastrarTurma da classe Turma

--- TrabalhoFinalOO/src/comunidade/Aluno.java
+++ TrabalhoFinalOO/src/comunidade/Aluno.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 public class Aluno extends Pessoa {
-	public int matriculaDoAluno;
+	public String matriculaDoAluno;
 	public int periodosDoAluno;
 	
 	public Aluno(){
@@ -18,7 +18,7 @@ public class Aluno extends Pessoa {
 //		}
 	}
 	
-	Aluno(String nomeDaPessoa, int idadeDaPessoa, int matriculaDoAluno, int periodosDoAluno){
+	Aluno(String nomeDaPessoa, int idadeDaPessoa, String matriculaDoAluno, int periodosDoAluno){
 		super(nomeDaPessoa, idadeDaPessoa);
 		this.matriculaDoAluno = matriculaDoAluno;
 		this.periodosDoAluno = periodosDoAluno;
@@ -28,7 +28,7 @@ public class Aluno extends Pessoa {
 	public static Aluno cadastrarAluno(Aluno novoAluno) {
 		
 		novoAluno.nomeDaPessoa = JOptionPane.showInputDialog("Digite o nome do Aluno");
-		novoAluno.matriculaDoAluno = Integer.parseInt(JOptionPane.showInputDialog("Digite a matricula do Aluno:"));
+		novoAluno.matriculaDoAluno = JOptionPane.showInputDialog("Digite a matricula do Aluno:");
 		
 		return novoAluno;
 	}
