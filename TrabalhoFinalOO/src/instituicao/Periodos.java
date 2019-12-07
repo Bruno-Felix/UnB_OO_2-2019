535f7436b357738a7ee0658135e2b7a792d0211f
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2019-12-07 16:16:48 -0300

    Update Periodos.java
    criando a verificaHorario,e alterando a listarPeriodos

--- TrabalhoFinalOO/src/instituicao/Periodos.java
+++ TrabalhoFinalOO/src/instituicao/Periodos.java
@@ -22,55 +22,57 @@ public class Periodos {
 	Object[] dias = {"Segunda", "Terca","Quarta","Quinta","Sexta","Sabado"};
 	String [] horas = {"00","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"};
     String [] minutos;
-    
+    int opcao,valido;
     
 	public void cadastrarPeriodos() {
+		do {
+			Periodos preenchePeriodo = new Periodos();
+			do {
 		
-		preencheArray();
-		
-		Periodos preenchePeriodo = new Periodos();
-		
-		preenchePeriodo.diaDaSemana = String.valueOf(JOptionPane.showInputDialog(null, "Qual o dia da semana?", "Escolha",
+				preencheArray();
+				preenchePeriodo.diaDaSemana = String.valueOf(JOptionPane.showInputDialog(null, "Qual o dia da semana?", "Escolha",
 						JOptionPane.QUESTION_MESSAGE, null, dias, dias[0]));
-		preenchePeriodo.horaInicio = String.valueOf(JOptionPane.showInputDialog(null, "Digite a hora de inicio", "Escolha",
+				preenchePeriodo.horaInicio = String.valueOf(JOptionPane.showInputDialog(null, "Digite a hora de inicio", "Escolha",
 				JOptionPane.QUESTION_MESSAGE, null, horas, horas[0]));
-		preenchePeriodo.minutosInicio = String.valueOf(JOptionPane.showInputDialog(null, "Digite os minutos de inicio", "Escolha",
+				preenchePeriodo.minutosInicio = String.valueOf(JOptionPane.showInputDialog(null, "Digite os minutos de inicio", "Escolha",
 				JOptionPane.QUESTION_MESSAGE, null, minutos, minutos[0]));
-		preenchePeriodo.horaTermino = String.valueOf(JOptionPane.showInputDialog(null, "Digite a hora de termino", "Escolha",
+				preenchePeriodo.horaTermino = String.valueOf(JOptionPane.showInputDialog(null, "Digite a hora de termino", "Escolha",
 				JOptionPane.QUESTION_MESSAGE, null, horas, horas[0]));
-		preenchePeriodo.minutosTermino = String.valueOf(JOptionPane.showInputDialog(null, "Digite os minutos de termino", "Escolha",
+				preenchePeriodo.minutosTermino = String.valueOf(JOptionPane.showInputDialog(null, "Digite os minutos de termino", "Escolha",
 				JOptionPane.QUESTION_MESSAGE, null, minutos, minutos[0]));
+				valido = verificaHorario(preenchePeriodo.horaInicio, preenchePeriodo.minutosInicio, preenchePeriodo.horaTermino,preenchePeriodo.minutosTermino,preenchePeriodo.diaDaSemana);
+			}while(valido == 0);
+			switch(preenchePeriodo.diaDaSemana) {
+				case "Segunda":
+					listaSegunda.add(preenchePeriodo);
+					break;
 			
-		//System.out.println("size1: " + listaSegunda.size());
-		switch(preenchePeriodo.diaDaSemana) {
-			case "Segunda":
-				listaSegunda.add(preenchePeriodo);
-				//System.out.println("size2: " + listaSegunda.size());
-			break;
+				case "Terca":
+					listaTerca.add(preenchePeriodo);
+				break;
 			
-			case "Terca":
-				listaTerca.add(preenchePeriodo);
-			break;
+				case "Quarta":
+					listaQuarta.add(preenchePeriodo);
+					break;
 			
-			case "Quarta":
-				listaQuarta.add(preenchePeriodo);
-			break;
+				case "Quinta":
+					listaQuinta.add(preenchePeriodo);
+				break;
 			
-			case "Quinta":
-				listaQuinta.add(preenchePeriodo);
-			break;
+				case "Sexta":
+					listaSexta.add(preenchePeriodo);
+					break;
 			
-			case "Sexta":
-				listaSexta.add(preenchePeriodo);
-			break;
+				case "Sabado":
+					listaSabado.add(preenchePeriodo);
+					break;
 			
-			case "Sabado":
-				listaSabado.add(preenchePeriodo);
-			break;
-			
-			default:
-			break;
-		}
+				default:
+				break;
+			}
+			opcao = JOptionPane.showConfirmDialog(null, "Quer continuar cadastrando periodos ?", "Escolha um",
+				JOptionPane.YES_NO_OPTION);
+		}while(opcao==0);
 	}
 	
 	public void listarPeriodos() {
@@ -78,10 +80,12 @@ public class Periodos {
 		for(int aux=0; aux<dias.length; aux++) {
 			switch(aux) {
 				case 0:
+					if(listaSegunda.size()>0) {
+						System.out.println(dias[aux]+"\n");
+					}
 					for(Periodos segunda : listaSegunda) {
 						if(segunda.horaInicio != null && segunda.horaTermino != null && segunda.minutosInicio != null && segunda.minutosTermino != null ) {
 							
-							System.out.println(dias[aux]+"\n");
 							System.out.println(segunda.horaInicio.toString() + ":" +segunda.minutosInicio.toString() + " ate " +
 									segunda.horaTermino.toString() + ":" + segunda.minutosTermino.toString());
 							System.out.println("\n");
@@ -89,10 +93,11 @@ public class Periodos {
 					}
 				break;
 				case 1:
+					if(listaTerca.size()>0) {
+						System.out.println(dias[aux]+"\n");
+					}
 					for(Periodos terca : listaTerca) {
 						if(terca.horaInicio != null && terca.horaTermino != null && terca.minutosInicio != null && terca.minutosTermino != null ) {
-							
-							System.out.println(dias[aux]+"\n");
 							System.out.println(terca.horaInicio.toString() + ":" +terca.minutosInicio.toString() + " ate " +
 									terca.horaTermino.toString() + ":" + terca.minutosTermino.toString());
 							System.out.println("\n");
@@ -100,10 +105,12 @@ public class Periodos {
 					}
 				break;
 				case 2:
+					if(listaQuarta.size()>0) {
+						System.out.println(dias[aux]+"\n");
+					}
 					for(Periodos quarta : listaQuarta) {
 						if(quarta.horaInicio != null && quarta.horaTermino != null && quarta.minutosInicio != null && quarta.minutosTermino != null ) {
 							
-							System.out.println(dias[aux]+"\n");
 							System.out.println(quarta.horaInicio.toString() + ":" +quarta.minutosInicio.toString() + " ate " +
 									quarta.horaTermino.toString() + ":" + quarta.minutosTermino.toString());
 							System.out.println("\n");
@@ -111,10 +118,12 @@ public class Periodos {
 					}
 				break;
 				case 3:
+					if(listaQuinta.size()>0) {
+						System.out.println(dias[aux]+"\n");
+					}
 					for(Periodos quinta : listaQuinta) {
 						if(quinta.horaInicio != null && quinta.horaTermino != null && quinta.minutosInicio != null && quinta.minutosTermino != null ) {
 							
-							System.out.println(dias[aux]+"\n");
 							System.out.println(quinta.horaInicio.toString() + ":" +quinta.minutosInicio.toString() + " ate " +
 									quinta.horaTermino.toString() + ":" + quinta.minutosTermino.toString());
 							System.out.println("\n");
@@ -122,10 +131,12 @@ public class Periodos {
 					}
 				break;
 				case 4:
+					if(listaSexta.size()>0) {
+						System.out.println(dias[aux]+"\n");
+					}
 					for(Periodos sexta : listaSexta) {
 						if(sexta.horaInicio != null && sexta.horaTermino != null && sexta.minutosInicio != null && sexta.minutosTermino != null ) {
 							
-							System.out.println(dias[aux]+"\n");
 							System.out.println(sexta.horaInicio.toString() + ":" +sexta.minutosInicio.toString() + " ate " +
 									sexta.horaTermino.toString() + ":" + sexta.minutosTermino.toString());
 							System.out.println("\n");
@@ -133,10 +144,12 @@ public class Periodos {
 					}
 				break;
 				case 5:
+					if(listaSabado.size()>0) {
+						System.out.println(dias[aux]+"\n");
+					}
 					for(Periodos sabado : listaSabado) {
 						if(sabado.horaInicio != null && sabado.horaTermino != null && sabado.minutosInicio != null && sabado.minutosTermino != null ) {
 							
-							System.out.println(dias[aux]+"\n");
 							System.out.println(sabado.horaInicio.toString() + ":" +sabado.minutosInicio.toString() + " ate " +
 									sabado.horaTermino.toString() + ":" + sabado.minutosTermino.toString());
 							System.out.println("\n");
@@ -149,6 +162,107 @@ public class Periodos {
 			}
 		}
 	}
+	
+	
+	
+	//Objetivo: verificar se esse periodo cadastrado ja existe
+	public int verificaHorario(String horaInicio, String minutosInicio,String horaTermino, String minutosTermino,String diaDaSemana) {
+		int	horaInicioMod = Integer.parseInt(horaInicio) * 60 + Integer.parseInt(minutosInicio);
+		int	horaTerminoMod = Integer.parseInt(horaTermino)* 60 + Integer.parseInt(minutosTermino);
+		switch(diaDaSemana) {
+			case "Segunda":
+				for(Periodos segunda : listaSegunda) {
+					int horaInicioLista = Integer.parseInt(segunda.horaInicio) * 60 + Integer.parseInt(segunda.minutosInicio);
+					int horaTerminoLista = Integer.parseInt(segunda.horaTermino) * 60 + Integer.parseInt(segunda.minutosTermino);
+					if(horaInicioLista >= horaInicioMod && horaInicioLista <= horaTerminoMod){
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+					else if(horaTerminoLista >= horaInicioMod && horaTerminoLista <= horaTerminoMod) {
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+				}
+			break;
+			case "Terca":
+				for(Periodos terca : listaTerca) {
+					int horaInicioLista = Integer.parseInt(terca.horaInicio) * 60 + Integer.parseInt(terca.minutosInicio);
+					int horaTerminoLista = Integer.parseInt(terca.horaTermino) * 60 + Integer.parseInt(terca.minutosTermino);
+					if(horaInicioLista >= horaInicioMod && horaInicioLista <= horaTerminoMod){
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+					else if(horaTerminoLista >= horaInicioMod && horaTerminoLista <= horaTerminoMod) {
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+				}
+			break;
+			case "Quarta":
+				for(Periodos quarta : listaQuarta) {
+					int horaInicioLista = Integer.parseInt(quarta.horaInicio) * 60 + Integer.parseInt(quarta.minutosInicio);
+					int horaTerminoLista = Integer.parseInt(quarta.horaTermino) * 60 + Integer.parseInt(quarta.minutosTermino);
+					if(horaInicioLista >= horaInicioMod && horaInicioLista <= horaTerminoMod){
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+					else if(horaTerminoLista >= horaInicioMod && horaTerminoLista <= horaTerminoMod) {
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+				}
+			break;
+			case "Quinta":
+				for(Periodos quinta : listaQuinta) {
+					int horaInicioLista = Integer.parseInt(quinta.horaInicio) * 60 + Integer.parseInt(quinta.minutosInicio);
+					int horaTerminoLista = Integer.parseInt(quinta.horaTermino) * 60 + Integer.parseInt(quinta.minutosTermino);
+					if(horaInicioLista >= horaInicioMod && horaInicioLista <= horaTerminoMod){
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+					else if(horaTerminoLista >= horaInicioMod && horaTerminoLista <= horaTerminoMod) {
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+				}
+			break;
+			case "Sexta":
+				for(Periodos sexta : listaSexta) {
+					int horaInicioLista = Integer.parseInt(sexta.horaInicio) * 60 + Integer.parseInt(sexta.minutosInicio);
+					int horaTerminoLista = Integer.parseInt(sexta.horaTermino) * 60 + Integer.parseInt(sexta.minutosTermino);
+					if(horaInicioLista >= horaInicioMod && horaInicioLista <= horaTerminoMod){
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+					else if(horaTerminoLista >= horaInicioMod && horaTerminoLista <= horaTerminoMod) {
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+				}
+			break;
+			case "Sabado":
+				for(Periodos sabado : listaSabado) {
+					int horaInicioLista = Integer.parseInt(sabado.horaInicio) * 60 + Integer.parseInt(sabado.minutosInicio);
+					int horaTerminoLista = Integer.parseInt(sabado.horaTermino) * 60 + Integer.parseInt(sabado.minutosTermino);
+					if(horaInicioLista >= horaInicioMod && horaInicioLista <= horaTerminoMod){
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+					else if(horaTerminoLista >= horaInicioMod && horaTerminoLista <= horaTerminoMod) {
+						JOptionPane.showMessageDialog(null, "Horario invalido, cadastre outro diferente");
+						return 0;
+					}
+				}
+			break;
+			default:
+				
+			break;
+		
+		
+		}
+		return 1;
+	}
+	
 	
 	public void preencheArray() {
 		
