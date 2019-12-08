4dcf03f9bf63c92fe953ae37c1085ee50b126bce
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2019-12-08 20:21:07 -0300

    Create ListenerDisciplina.java
    criando o listenerDisciplina

---
+++ TrabalhoFinalOO/src/aplicacao/ListenerDisciplina.java
@@ -0,0 +1,11 @@
+package aplicacao;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class ListenerDisciplina implements ActionListener{
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		FormDisciplina formDisciplina = new FormDisciplina();
+	}
+}
