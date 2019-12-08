13019b0f28d59b06bbd785b18f6c4afcb640d6dc
Bruno-Felix <balvesfelix@gmail.com>
2019-12-08 19:22:35 -0300

    Ajustando layout Interface Campus

--- TrabalhoFinalOO/src/aplicacao/FormCampus.java
+++ TrabalhoFinalOO/src/aplicacao/FormCampus.java
@@ -24,16 +24,26 @@ public class FormCampus extends JFrame {
 	public FormCampus() {
 		
 		setBounds(800, 200, 480, 300);
-		setLayout(new GridLayout(3, 3)); //nesse caso, uma tabela 3x3
+		setLayout(null); //nesse caso, uma tabela 3x3
 		setTitle("Campos");
 		
-		btnCriarCampus = new JButton("Criar Turma");
-		btnCriarCampus.addActionListener(new ActionListenerbtnCriarCampus());
-		add(btnCriarCampus);
-		
-		btnListarCampus = new JButton("Listar Turma");
-		btnListarCampus.addActionListener(new ActionListenerbtnListarCampus());
-		add(btnListarCampus);
+//		btnCriarCampus = new JButton("Criar Turma");
+//		btnCriarCampus.addActionListener(new ActionListenerbtnCriarCampus());
+//		add(btnCriarCampus);
+//		
+//		btnListarCampus = new JButton("Listar Turma");
+//		btnListarCampus.addActionListener(new ActionListenerbtnListarCampus());
+//		add(btnListarCampus);
+
+        btnCriarCampus = new JButton("Criar Turma");
+        btnCriarCampus.setBounds(22, 81, 132, 23);
+        btnCriarCampus.addActionListener(new ActionListenerbtnCriarCampus());
+        add(btnCriarCampus);
+
+        btnListarCampus = new JButton("Listar Turma");
+        btnListarCampus.setBounds(258, 81, 142, 23);
+        btnListarCampus.addActionListener(new ActionListenerbtnListarCampus());
+        add(btnListarCampus);
 		
 			
 		setVisible(true);
