1d465cc240caa34a1f654c03986a876009275914
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2019-12-09 00:21:45 -0300

    Create PredioNaoCriadoException.java
    criando exceção de predio nao criado

---
+++ TrabalhoFinalOO/src/excessoes/PredioNaoCriadoException.java
@@ -0,0 +1,22 @@
+package excessoes;
+import instituicao.*;
+public class PredioNaoCriadoException {
+	public static void PredioNCriadoException() {
+		try {
+			Predio.criarSalaNoPredio();
+		}
+		catch(ArrayIndexOutOfBoundsException  e){
+			System.out.println("---------------------");
+			System.out.println("# ERRO: Nao existe um predio cadastrado para atribuir a essa sala.\n");
+
+			System.out.println("# Por Favor, crie um Predio para atribui-lo a Sala.\n");
+			CampusNaoCriadoException.CampusNCriadoException();
+			Predio.criarSalaNoPredio();
+		}
+		
+		
+		
+	}
+	
+	
+}
