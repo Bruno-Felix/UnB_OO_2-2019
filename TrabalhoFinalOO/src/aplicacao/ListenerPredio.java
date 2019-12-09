179e1309fe6dd00c8a1efec4f3dc88cec21d7037
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2019-12-08 22:06:24 -0300

    Create ListenerPredio.java
    criando listenerPredio

---
+++ TrabalhoFinalOO/src/aplicacao/ListenerPredio.java
@@ -0,0 +1,9 @@
+package aplicacao;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class ListenerPredio implements ActionListener{
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		FormPredio formpredio = new FormPredio();
+	}
+}
