c75c75baec64dde1a2ef3a5a43a3a798fea7959d
Bruno-Felix <balvesfelix@gmail.com>
2019-12-09 09:55:55 -0300

    Retirando códico comentado e deixando a mainApp pronta pra rodar a Interface Gráfica

--- TrabalhoFinalOO/src/aplicacao/App.java
+++ TrabalhoFinalOO/src/aplicacao/App.java
@@ -1,38 +1,11 @@
 package aplicacao;
 
-import instituicao.*;
-import comunidade.*;
-
 public class App {
 
 	private static menuPrincipal form; 
 	
 	public static void main(String[] args) {
-			
-		//Periodos.cadastrarPeriodos();
-		
-		Ocupacao.criarECadastrarOcupacao();
-		
-		//form = new menuPrincipal();
-		
-//		Campus.criarCampus();
-//		Campus.listarCampus();
-//		Campus.criarPredio();
-//		Predio.listarPredios();
-//		Predio.criarSalaNoPredio();
-//		Sala.listarSalasDeUmPredio();
-			
-//		Campus.criarProfessorCasoTeste();
-//		Aluno.cadastrarAlunoCasoTeste();
-//		Disciplina.criarDisciplina();
-//		Disciplina.listarDisciplinas();
-//		Disciplina.alterarDisciplina();
-//		Disciplina.listarDisciplinas();
-//		Disciplina.criarTurma();
-//		Turma.listarAlunosDaTurma();
-//		Turma.listarTurmas();
-//		Aluno.listarTurmasDoAluno();
-		
-		//viewMenuPrincipal interface = new viewMenuPrincipal();
+					
+		form = new menuPrincipal();
 	}
 }
