7bb5725425130187fd6b576d3190f7f173c2266f
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2019-12-10 23:18:55 -0300

    Update Ocupacao.java
    criando a listarOcupacoes

--- TrabalhoFinalOO/src/instituicao/Ocupacao.java
+++ TrabalhoFinalOO/src/instituicao/Ocupacao.java
@@ -415,7 +415,30 @@ public class Ocupacao {
 	}
 
 	public static void listaDeOcupacoes() {
-		// TODO Auto-generated method stub
+		
+		for (Ocupacao ocupacao : listaDeOcupacoes) {
+		      String auxDia = String
+						      .valueOf(JOptionPane.showInputDialog(null, "Qual dia das ocupacoes a serem listadas?", "Escolha",
+								   JOptionPane.QUESTION_MESSAGE, null, dias, dias[0]));
+		      if(ocupacao.periodoDaOcupacao.diaDaSemana.equalsIgnoreCase(auxDia)){
+		    	  Object[] objArrayPeriodos = listaDeOcupacoes.toArray();
+		    	  for(int i = 0; i < objArrayPeriodos.length; i++) {
+		    		  objArrayPeriodos[i] = ocupacao.periodoDaOcupacao.horaInicio + " : " + ocupacao.periodoDaOcupacao.minutosInicio + " - " + ocupacao.periodoDaOcupacao.horaTermino + " : " + ocupacao.periodoDaOcupacao.minutosTermino;
+		    	  }
+		          String auxHorario = String
+		    				      .valueOf(JOptionPane.showInputDialog(null, "Qual horario das ocupacoes a serem listadas?", "Escolha",
+		    						   JOptionPane.QUESTION_MESSAGE, null, objArrayPeriodos, objArrayPeriodos[0]));
+		                   if(ocupacao.periodoDaOcupacao.horaInicio.equals(auxHorario.substring(0,1))){
+		                   
+		                	   System.out.println("Sala: " + ocupacao.salaDaOcupacao);
+		                	   System.out.println("Disciplina: " + ocupacao.turmaDaOcupacao.disciplinaDaTurma);
+		                	   System.out.println("Turma: " + ocupacao.turmaDaOcupacao);
+		                	   System.out.println("Professor: " + ocupacao.turmaDaOcupacao.professorDaTurma);
+		                	   System.out.println("Horario: " + auxHorario); 
+		                   }
+		                   
+		      }
+		}
 		
 	}
 }
